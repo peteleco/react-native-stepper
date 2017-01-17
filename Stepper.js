50a60fe56d8a169b864e2b034d183635e6902700
Leonardo Jorge <ldiascarmo@gmail.com>
2017-01-17 18:07:16 -0200

    Basic setup.  Increase and decrease with min and max validations.

--- Stepper.js
+++ Stepper.js
@@ -1,25 +1,138 @@
 import React, { Component, PropTypes } from 'react'
-import { StyleSheet, Text, TouchableHighlight, Image, View } from 'react-native'
+import { Alert, StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native'
 
 class Stepper extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      value: this.props.initValue,
+      value: props.initValue, //this.props.initValue
       hasReachedMinValue: false,
       hasReachedMaxValue: false,
     };
+
+    this.validateInitValueAgainstMaxValue(props.initValue, props.maxValue);
+    this.validateInitValueAgainstMinValue(props.initValue, props.minValue);
+    if(!props.ignoreMaxValidation) {
+        this.validateRangeMaxValue(props.initValue, props.stepValue, props.maxValue);
+    }
+    if(!props.ignoreMinValidation) {
+        this.validateRangeMinValue(props.initValue, props.stepValue, props.minValue);
+    }
+  }
+
+  // Checks if initValue are lower than maxValue
+  validateInitValueAgainstMaxValue(initValue, maxValue) {
+    if(maxValue !== null && initValue > maxValue) {
+      throw "The initital value can't be higher than max value";
+    }
+    return true;
+  }
+
+  // Checks if minValue are lower than maxValue
+  validateInitValueAgainstMinValue(initValue, minValue) {
+    if(minValue !== null && initValue < minValue) {
+      throw "The initital value can't be lower than min value";
+    }
+    return true;
+  }
+
+  // Checks if range between initalValue and MaxValue are valids
+  validateRangeMaxValue(initValue, stepValue, maxValue) {
+    if(maxValue !== null && (((maxValue - initValue) % stepValue) !== 0)) {
+      throw "The initital value or step value are not valids with this max value";
+    }
+    return true;
+  }
+
+  // Checks if range between initalValue and MinValue are valids
+  validateRangeMinValue(initValue, stepValue, minValue) {
+    if(minValue !== null && (((initValue - minValue) % stepValue) !== 0)) {
+      throw "The initital value or step value are not valids with this min value";
+    }
+    return true;
+  }
+
+  // Check if reached max value
+  hasReachedMaxValue(value) {
+    if(this.props.maxValue === null) {
+      return false;
+    }
+
+    return (value > this.props.maxValue);
+  }
+
+  // Check if reached min value
+  hasReachedMinValue(value) {
+    if(this.props.minValue === null) {
+      return false;
+    }
+
+    return (value < this.props.minValue);
+  }
+
+
+  // Increase value
+  increase(stepValue) {
+    const { state } = this;
+    const currentValue = state.value + stepValue;
+    if(this.hasReachedMaxValue(currentValue)) {
+      // fire alert function passed by
+      return Alert.alert('Máximo atingido');
+    }
+
+    this.setState({...state, value:currentValue});
+    this.valueChanged(currentValue);
+  }
+
+  // Deacrease value
+  decrease(stepValue) {
+    const { state } = this;
+    const currentValue = state.value - stepValue;
+    if(this.hasReachedMinValue(currentValue)) {
+      // fire alert function passed by
+      return Alert.alert('Minimo atingido');
+    }
+    this.setState({...state, value: currentValue});
+    this.valueChanged(currentValue);
+  }
+
+  // On press increase button
+  onPressIncreaseButton() {
+    this.increase(this.props.stepValue);
+  }
+
+  // On press decrease button
+  onPressDecreaseButton() {
+    this.decrease(this.props.stepValue);
+  }
+
+  // Value changed: use this function to get value
+  valueChanged(value) {
+    if (this.props.valueChanged) {
+      this.props.valueChanged(value)
+    }
   }
 
   render() {
     return (
       <View>
+        <TouchableOpacity onPress={this.onPressDecreaseButton.bind(this)}><Text>-</Text></TouchableOpacity>
         <Text>Oi: {this.state.value}</Text>
+        <TouchableOpacity onPress={this.onPressIncreaseButton.bind(this)}><Text>+</Text></TouchableOpacity>
       </View>
     );
   }
 
 }
+
+Stepper.defaultProps = {
+  initValue: 0,
+  minValue: null,
+  maxValue: null,
+  ignoreMinValidation: false,
+  ignoreMaxValidation: false,
+  valueChanged: null
+};
 
 Stepper.propTypes = {
     // Min value, if null is unlimited
@@ -27,7 +140,12 @@ Stepper.propTypes = {
     // Max value, if null is unlimited
     maxValue: React.PropTypes.number,
     // Initial value
-    initValue: React.PropTypes.number,
+    initValue: React.PropTypes.number.isRequired,
     // Step value
-    stepValue: React.PropTypes.number,
+    stepValue: React.PropTypes.number.isRequired,
+    ignoreMinValidation: React.PropTypes.bool,
+    ignoreMaxValidation: React.PropTypes.bool,
+    valueChanged: React.PropTypes.func
 };
+
+export default Stepper;
